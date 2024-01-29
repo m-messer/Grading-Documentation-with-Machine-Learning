499a143875014d12b645b10d093097991375d804
Marcus Messer <marcus@marcusm.co.uk>
2024-01-29 18:34:46 +0000

    PHD-664 Complete first draft of CoT approach

--- training/metrics.py
+++ training/metrics.py
@@ -59,6 +59,28 @@ def compute_metrics_trad(predictions, prediction_prob, labels):
             'precision_micro': precision_micro_res, 'precision_weighted': precision_weighted_res,
             'loss': loss
             }
+def compute_metrics_cot(predictions, labels):
+    accuracy_res = accuracy.compute(predictions=predictions, references=labels)['accuracy']
+    f1_macro_res = f1.compute(predictions=predictions, references=labels, average='macro')['f1']
+    f1_micro_res = f1.compute(predictions=predictions, references=labels, average='micro')['f1']
+    f1_weighted_res = f1.compute(predictions=predictions, references=labels, average='weighted')['f1']
+    recall_macro_res = recall.compute(predictions=predictions, references=labels, average='macro')['recall']
+    recall_micro_res = recall.compute(predictions=predictions, references=labels, average='micro')['recall']
+    recall_weighted_res = recall.compute(predictions=predictions, references=labels, average='weighted')[
+        'recall']
+    precision_macro_res = precision.compute(predictions=predictions, references=labels, average='macro')[
+        'precision']
+    precision_micro_res = precision.compute(predictions=predictions, references=labels, average='micro')[
+        'precision']
+    precision_weighted_res = precision.compute(predictions=predictions, references=labels, average='weighted')[
+        'precision']
+
+    return {'accuracy': accuracy_res,
+            'f1_macro': f1_macro_res, 'f1_micro': f1_micro_res, 'f1_weighted': f1_weighted_res,
+            'recall_macro': recall_macro_res, 'recall_micro': recall_micro_res,
+            'recall_weighted': recall_weighted_res, 'precision_macro': precision_macro_res,
+            'precision_micro': precision_micro_res, 'precision_weighted': precision_weighted_res,
+            }
 
 
 def format_metrics(metrics, prefix):
--- training/training_CoT.py
+++ training/training_CoT.py
@@ -1,9 +1,13 @@
 from functools import partial
 
+import optuna
 from openai import OpenAI
 
+import asyncio
 import wandb
+import random
 from data_processing.data_processor import get_data
+from training.metrics import compute_metrics_cot
 
 """
 TODO:
@@ -19,48 +23,108 @@ TODO:
 
 class Train:
     PROMPT = 'Take the description "{nl}" and the code "{code}" and classify how relevant the description is to ' \
-                  'the code between 0 and 3.'
+             'the code between 0 and 3.'
 
-    def __init__(self, data_dir, wandb_project, preprocess=False):
+    def __init__(self, data_dir, wandb_project, preprocess=False, number_of_examples=5, epochs=10):
         with open('../secrets/wandb_api_key.txt') as f:
             wandb.login(key=f.read())
 
+        self.client = OpenAI()
+
         self.model = 'gpt-3.5-turbo'
+        self.number_of_examples = number_of_examples
+        self.epochs = epochs
 
         self.wandb_project = wandb_project
         self.data = get_data(data_dir=data_dir, pre_process=preprocess)
+        self.data = self.data.map(partial(self.__create_prompt,
+                                          prompt=self.PROMPT))
 
-        self.train_test_data = self.data.train_test_split(test_size=0.2)
-        self.train_test_data['train'] = self.train_test_data['train'].map(partial(self.__create_prompt,
-                                                                                  prompt=self.PROMPT))
-        self.train_test_data['test'] = self.train_test_data['test'].map(partial(self.__create_prompt,
-                                                                                prompt=self.PROMPT))
+        self.predictions = []
+        self.labels = []
 
     def __create_prompt(self, row, prompt):
         row['prompt'] = prompt.format(nl=row['query'], code=row['func_code_string'], label=row['label'])
 
         return row
 
-    def send_gpt_request(self):
-        client = OpenAI()
+    def __make_messages(self):
+        messages = [
+            {'role': 'system', 'content': 'You will be provided a description and a piece of code, and your task '
+                                          'is to classify is between 0 and 3.'},
+        ]
 
-        # TODO: How to format messages to GPT for CoT? Any examples?
+        random_sample = random.sample(range(0, self.data.shape[0]), self.number_of_examples + 1)
+        random_sample_df = self.data.select(random_sample)
 
-        completion = client.chat.completions.create(
+        for i in range(0, self.number_of_examples):
+            messages.append({'role': 'user',
+                             'content': random_sample_df[i]['prompt'] + 'The answer is ' +
+                                        str(random_sample_df[i]['label'])})
+
+        messages.append({'role': 'user',
+                         'content': random_sample_df[self.number_of_examples]['prompt']})
+
+        test_label = random_sample_df[self.number_of_examples]['label']
+
+        return messages, test_label
+
+    def __format_results(self, response, label):
+        pred = [int(s) for s in response.replace('.', '').split() if s.isdigit()][0]
+        self.predictions.append(pred)
+        self.labels.append(label)
+
+    async def send_gpt_request(self):
+
+        messages, test_label = self.__make_messages()
+
+        completion = self.client.chat.completions.create(
             model=self.model,
-            messages=[
-                {'role': 'system', 'content': 'You will be provided a description and a piece of code, and your task '
-                                              'is to classify is between 0 and 3.'},
-                {'role': 'user', 'content': self.train_test_data['train'][0]['prompt']},
-                {'role': 'assistant', 'content': 'The answer is {label}'.format(label=self.train_test_data['train'][0]['label'])},
-                {'role': 'user', 'content': self.train_test_data['train'][1]['prompt']},
-            ]
+            messages=messages
         )
 
-        print(completion.choices[0].message.content)
+        response = completion.choices[0].message.content
 
+        self.__format_results(response, test_label)
+
+    async def train(self, trial):
+        tags = ['DEV']
+
+        number_of_examples = trial.suggest_categorical('number_of_examples', [3, 5, 10])
+        self.number_of_examples = number_of_examples
+
+        config = dict(trial.params)
+        config['trial.number'] = trial.number
+        config['epochs'] = self.epochs
+
+        wandb.init(
+            project=self.wandb_project,
+            group="CoT:" + self.model,
+            tags=tags,
+            reinit=True,
+            config=config
+        )
+
+        for i in range(0, self.epochs):
+            await self.send_gpt_request()
+
+            metrics = compute_metrics_cot(self.predictions, self.labels)
+            results_formatted = {"test/" + key: item for key, item in metrics.items()}
+            results_formatted['epoch'] = i
+            print(results_formatted)
+            wandb.log(results_formatted)
+
+            # Wait to not spam the GPT API with requests
+            await asyncio.sleep(10)
+
+        return metrics['accuracy']
+    def objective(self, trial):
+        return asyncio.run(train.train(trial))
 
 if __name__ == '__main__':
-    train = Train(data_dir='../data/code_search_net_relevance.hf', wandb_project='')
+    train = Train(data_dir='../data/code_search_net_relevance.hf', wandb_project='JavaDoc-Relevance-Binary-Classifier')
 
-    train.send_gpt_request()
+    study = optuna.create_study(direction='maximize')
+    study.optimize(train.objective, n_trials=5)
+
+
