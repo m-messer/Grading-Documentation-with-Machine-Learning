ebb6ebe3c04e7b6a120d781f0bc95217a303c42f
Marcus Messer <marcus@marcusm.co.uk>
2024-07-26 13:06:00 -0400

    PHD-738 Removed combination sampling

--- sampling.py
+++ sampling.py
@@ -1,8 +1,7 @@
 
 from imblearn.over_sampling import RandomOverSampler, SMOTE, ADASYN
-from imblearn.combine import SMOTEENN, SMOTETomek
 
-VALID_SAMPLING_VALUES = ['RandomOverSample', 'SMOTE', 'ADASYN', 'SMOTEENN', 'SMOTETomek']
+VALID_SAMPLING_VALUES = ['RandomOverSample', 'SMOTE', 'ADASYN']
 
 def sample_data(train_val_X, train_val_y, sampling_method):
     assert sampling_method in VALID_SAMPLING_VALUES
@@ -15,9 +14,3 @@ def sample_data(train_val_X, train_val_y, sampling_method):
     elif sampling_method == 'ADASYN':
         ada = ADASYN(random_state=0)
         return ada.fit_resample(train_val_X, train_val_y)
-    elif sampling_method == 'SMOTEENN':
-        smoteen = SMOTEENN(random_state=0)
-        return smoteen.fit_resample(train_val_X, train_val_y)
-    elif sampling_method == 'SMOTETomek':
-        smotetomek = SMOTETomek(random_state=0)
-        return smotetomek.fit_resample(train_val_X, train_val_y)
