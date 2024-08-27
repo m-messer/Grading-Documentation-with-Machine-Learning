43453d1cdeacc08af75107aa4dbb9ea920066a7a
Marcus Messer <marcus@marcusm.co.uk>
2024-08-27 11:11:37 +0100

    Revert to AIED version - Revert "PHD-738 Removed combination sampling"
    This reverts commit ebb6ebe3c04e7b6a120d781f0bc95217a303c42f.

--- sampling.py
+++ sampling.py
@@ -1,7 +1,8 @@
 
 from imblearn.over_sampling import RandomOverSampler, SMOTE, ADASYN
+from imblearn.combine import SMOTEENN, SMOTETomek
 
-VALID_SAMPLING_VALUES = ['RandomOverSample', 'SMOTE', 'ADASYN']
+VALID_SAMPLING_VALUES = ['RandomOverSample', 'SMOTE', 'ADASYN', 'SMOTEENN', 'SMOTETomek']
 
 def sample_data(train_val_X, train_val_y, sampling_method):
     assert sampling_method in VALID_SAMPLING_VALUES
@@ -14,3 +15,9 @@ def sample_data(train_val_X, train_val_y, sampling_method):
     elif sampling_method == 'ADASYN':
         ada = ADASYN(random_state=0)
         return ada.fit_resample(train_val_X, train_val_y)
+    elif sampling_method == 'SMOTEENN':
+        smoteen = SMOTEENN(random_state=0)
+        return smoteen.fit_resample(train_val_X, train_val_y)
+    elif sampling_method == 'SMOTETomek':
+        smotetomek = SMOTETomek(random_state=0)
+        return smotetomek.fit_resample(train_val_X, train_val_y)
