629035737c7759d73957f673e2cbfcf63570e257
Marcus Messer <marcus@marcusm.co.uk>
2025-01-21 11:10:58 +0000

    PHD-898 Oversampled menagerie

--- data_processing/data_processor.py
+++ data_processing/data_processor.py
@@ -1,7 +1,10 @@
 from datasets import Dataset, interleave_datasets, load_dataset
 
+GRADES = ['F', 'D-', 'D', 'D+', 'C-', 'C', 'C+', 'B-', 'B', 'B+', 'A-', 'A', 'A+', 'A++']
+
 
 def get_label_info(binary: bool):
+    # TODO: Update for Menagerie
     """
     Gets the ID, label and label count dependant if the model training is binary or multi-class.
     :param binary: If the model training is multiclass or not
@@ -55,21 +58,50 @@ def get_data(data_dir: str, binary: bool = False):
     return data
 
 
-def over_sample(original_dataset):
-    # TODO: Update to include grades for menagrie or refactor name and create new function?
+def over_sample_menagerie(original_dataset, binary):
+    if not binary:
+        filtered_datasets = []
+        for grade in GRADES:
+            temp_dataset = original_dataset.filter(lambda row: row['label'] == grade)
+
+            if len(temp_dataset) > 0:
+                filtered_datasets.append(temp_dataset)
+
+        over_sampled_train_data = interleave_datasets(filtered_datasets,
+                                                      seed=100, stopping_strategy='all_exhausted')
+    else:
+        class_0_data = original_dataset.filter(lambda row: row['label'] == 0)
+        class_1_data = original_dataset.filter(lambda row: row['label'] == 1)
+        over_sampled_train_data = interleave_datasets([class_0_data, class_1_data],
+                                                      seed=100, stopping_strategy='all_exhausted')
+
+    return over_sampled_train_data
+
+
+def over_sample_code_search_net(original_dataset, binary):
     class_0_data = original_dataset.filter(lambda row: row['label'] == 0)
     class_1_data = original_dataset.filter(lambda row: row['label'] == 1)
     class_2_data = original_dataset.filter(lambda row: row['label'] == 2)
     class_3_data = original_dataset.filter(lambda row: row['label'] == 3)
 
-    over_sampled_train_data = interleave_datasets([class_0_data, class_1_data, class_2_data, class_3_data],
-                               seed=100, stopping_strategy='all_exhausted')
-    # over_sampled_train_data = interleave_datasets([class_0_data, class_1_data],
-    #                                               seed=100, stopping_strategy='all_exhausted')
+    if not binary:
+        over_sampled_train_data = interleave_datasets([class_0_data, class_1_data, class_2_data, class_3_data],
+                                                      seed=100, stopping_strategy='all_exhausted')
+    else:
+        over_sampled_train_data = interleave_datasets([class_0_data, class_1_data],
+                                                      seed=100, stopping_strategy='all_exhausted')
     return over_sampled_train_data
 
 
+def over_sample(original_dataset, dataset_name, binary=False):
+    if dataset_name == 'CodeSearchNet':
+        return over_sample_code_search_net(original_dataset, binary)
+    elif dataset_name == 'Menagerie':
+        return over_sample_menagerie(original_dataset, binary)
+
+
 def __convert_to_binary(row):
+    # TODO: Update for Menagerie
     if row['label'] in [0, 1]:
         row['label'] = 0
     if row['label'] in [2, 3]:
@@ -83,6 +115,12 @@ if __name__ == '__main__':
     print('Menagerie')
     print(menagerie_df)
     print(menagerie_df[0])
+    oversampled_menagerie_df = over_sample(menagerie_df, dataset_name='Menagerie', binary=False)
+    print(oversampled_menagerie_df)
+    print(oversampled_menagerie_df.to_pandas()['label'].value_counts())
     csn_df = get_data(data_dir='../data/code_search_net_relevance.hf')
     print('CodeSearchNet')
     print(csn_df)
+    oversampled_csn_df = over_sample(csn_df, dataset_name='CodeSearchNet')
+    print(oversampled_csn_df)
+    print(oversampled_csn_df.to_pandas()['label'].value_counts())
